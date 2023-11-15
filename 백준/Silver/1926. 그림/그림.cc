ad27f4fbd42186d8e1cce397665a9ac488ab0d41
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2023-11-15 23:33:40 +0900

    [Silver I] Title: 그림, Time: 32 ms, Memory: 4004 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-int a[504][504], visited[504][504], n, m, x, y;
+int dy[] = {0, 1, 0, -1};
+int dx[] = {1, 0, -1, 0};
+int a[504][504], visited[504][504], n, m, x, y, nx, ny, cnt, mx;
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> a[i][j];
         }
     }
-    int mx = 0;
-    int cnt = 0;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             if(a[i][j] == 0 || visited[i][j]) continue;
@@ -27,9 +28,9 @@ int main() {
                 tie(y, x) = q.front();
                 q.pop();
                 for(int i = 0; i < 4; i++) {
-                    int ny = y + dy[i];
-                    int nx = x + dx[i];
-                    if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+                    ny = y + dy[i];
+                    nx = x + dx[i];
+                    if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
                     if(visited[ny][nx] || a[ny][nx] == 0) continue;
                     visited[ny][nx] = 1;
                     q.push({ny, nx});
@@ -38,5 +39,6 @@ int main() {
             mx = max(mx, area);
         }
     }
+
     cout << cnt << '\n' << mx;
 }
