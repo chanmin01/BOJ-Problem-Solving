0c1a6c6a35fb401aba0d30ad689c24c2db8eb8b9
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2023-11-16 13:56:39 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -3,10 +3,13 @@ using namespace std;
 
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-int a[1004][1004], visited[1004][1004], x, y, m, n;
+int a[1004][1004], visited[1004][1004], m, n, x, y, nx, ny;
+
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> m >> n;
-    int flag = 1;
     queue<pair<int, int>> q;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
@@ -17,13 +20,14 @@ int main() {
             }
         }
     }
+ 
     while(q.size()) {
         tie(y, x) = q.front();
         q.pop();
         for(int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            ny = y + dy[i];
+            nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
             if(visited[ny][nx] || a[ny][nx] == -1) continue;
             visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
@@ -33,12 +37,13 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             if(!visited[i][j] && a[i][j] == 0) {
-                flag = 0;
-                break;
+                cout << -1;
+                return 0;
             }
             cnt = max(cnt, visited[i][j]);
         }
     }
-    if(flag) cout << cnt - 1 << '\n';
-    else cout << -1 << '\n';
+    cout << cnt - 1;
+    return 0;
+
 }
