4236b4d2a55dae6838a84c652422291a920c3a50
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-01-10 00:38:27 +0900

    [Silver II] Title: 나무 자르기, Time: 672 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int m, n, a[1000004];
+
+bool solve(ll x) {
+    ll cnt = 0;
+    for(int i = 0; i < n; i++) {
+        if(a[i] - x > 0) {
+            cnt += a[i] - x;
+        }
+    }
+    return cnt >= m;
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    ll st = 1;
+    ll en = 2000000000;
+    sort(a, a + n);
+    while(st <= en) {
+        ll mid = (st + en) / 2;
+        if(solve(mid)) st = mid + 1;
+        else en = mid - 1;
+    }
+    cout << en;
+}
