a89bc34eac05403a49a5533724e2b4a508c0adc9
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-03-30 14:03:22 +0900

    [Gold IV] Title: N-Queen, Time: 1620 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt, a[17][17], visited1[40], visited2[40], visited3[40], n;
+int visited1[40], visited2[40], visited3[40];
 
-void func(int m) {
-    if(m == n) {
+int cnt = 0;
+int n;
+void func(int cur) {
+    if (cur == n) {
         cnt++;
         return;
     }
+
     for(int i = 0; i < n; i++) {
-        if(visited1[i] || visited2[i + m] || visited3[i - m + n - 1]) continue;
+        if(visited1[i] || visited2[i + cur] || visited3[i - cur + n - 1]) continue;
         visited1[i] = 1;
-        visited2[i + m] = 1;
-        visited3[i - m + n - 1] = 1;
-        func(m + 1);
+        visited2[i + cur] = 1;
+        visited3[i - cur + n - 1] = 1;
+        func(cur + 1);
         visited1[i] = 0;
-        visited2[i + m] = 0;
-        visited3[i - m + n - 1] = 0;
+        visited2[i + cur] = 0;
+        visited3[i - cur + n - 1] = 0;
     }
+
 
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
     cin >> n;
     func(0);
-    cout << cnt << '\n';
+    cout << cnt;
 }
