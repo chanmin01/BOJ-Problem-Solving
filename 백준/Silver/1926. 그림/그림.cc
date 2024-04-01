3249664f10e2c37e25991e372285177635acb671
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-04-01 13:45:08 +0900

    [Silver I] Title: 그림, Time: 28 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,44 +1,48 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int dy[] = {0, 1, 0, -1};
-int dx[] = {1, 0, -1, 0};
-int a[504][504], visited[504][504], n, m, x, y, nx, ny, cnt, mx;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int board[505][505], visited[505][505], cnt, mx;
+int n, m;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
     cin >> n >> m;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cin >> a[i][j];
-        }
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(a[i][j] == 0 || visited[i][j]) continue;
-            int area = 0;
-            cnt++;
-            visited[i][j] = 1;
-            queue<pair<int, int>> q;
-            q.push({i, j});
-            while(q.size()) {
-                area++;
-                tie(y, x) = q.front();
-                q.pop();
-                for(int i = 0; i < 4; i++) {
-                    ny = y + dy[i];
-                    nx = x + dx[i];
-                    if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-                    if(visited[ny][nx] || a[ny][nx] == 0) continue;
-                    visited[ny][nx] = 1;
-                    q.push({ny, nx});
-                }
-            }
-            mx = max(mx, area);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
         }
     }
 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!visited[i][j] && board[i][j] == 1) {
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                visited[i][j] = 1;
+                int curArea = 0;
+                while (!q.empty()) {
+                    int y = q.front().first;
+                    int x = q.front().second;
+                    q.pop();
+                    curArea++;
+                    for (int k = 0; k < 4; k++) {
+                        int ny = y + dy[k];
+                        int nx = x + dx[k];
+                        if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || !board[ny][nx]) continue;
+                        visited[ny][nx] = 1;
+                        q.push({ny, nx});
+                    }
+                }
+                cnt++;
+                mx = max(mx, curArea);
+            }
+        }
+    }
     cout << cnt << '\n' << mx;
+    return 0;
 }
