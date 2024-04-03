f29d28c614c90c210f32a71afcf7a515245666ad
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-04-03 09:44:19 +0900

    [Gold V] Title: 토마토, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,49 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int m, n, board[1004][1004], visited[1004][1004], x, y;
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-int a[1004][1004], visited[1004][1004], m, n, x, y, nx, ny;
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
     cin >> m >> n;
     queue<pair<int, int>> q;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin >> a[i][j];
-            if(a[i][j] == 1) {
+            cin >> board[i][j];
+            if(board[i][j] == 1) {
                 q.push({i, j});
                 visited[i][j] = 1;
             }
         }
     }
- 
+
     while(q.size()) {
         tie(y, x) = q.front();
         q.pop();
         for(int i = 0; i < 4; i++) {
-            ny = y + dy[i];
-            nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if(visited[ny][nx] || a[ny][nx] == -1) continue;
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || board[ny][nx] == -1) continue;
             visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
         }
     }
-    int cnt = 0;
+
+    bool is_all = true;
+    int mx = -1;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(!visited[i][j] && a[i][j] == 0) {
-                cout << -1;
-                return 0;
-            }
-            cnt = max(cnt, visited[i][j]);
+            if(visited[i][j] > mx) mx = max(mx, visited[i][j]);
+            if(board[i][j] == 0 && !visited[i][j]) is_all = false;
         }
     }
-    cout << cnt - 1;
-    return 0;
 
+    if(is_all)  {
+        cout << mx - 1 << '\n';
+    } else {
+        cout << -1 << '\n';
+    }
+    
 }
