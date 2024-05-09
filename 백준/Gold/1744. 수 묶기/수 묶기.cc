3fd463544a639795e75098cb388112809d87ad10
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-05-09 18:04:26 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.cc
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> positive, negative;
+int n;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n;
     cin >> n;
-    vector<int> positive, negative;
-
-    for (int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; i++) {
         int num;
         cin >> num;
-        if (num > 0)
+        if(num > 0) {
             positive.push_back(num);
-        else
+        } else {
             negative.push_back(num);
+        }
     }
 
     sort(positive.begin(), positive.end(), greater<int>());
@@ -23,25 +22,23 @@ int main() {
 
     int sum = 0;
 
-    for (int i = 0; i < positive.size(); ++i) {
-        if (i + 1 < positive.size() && positive[i + 1] > 1) {
+    for(int i = 0; i < positive.size(); i++) {
+        if(i + 1 < positive.size() && positive[i + 1] > 1) {
             sum += positive[i] * positive[i + 1];
             ++i;
-        }
-        else
+        } else {
             sum += positive[i];
+        }
     }
 
-    for (int i = 0; i < negative.size(); ++i) {
-        if (i + 1 < negative.size()) {
+    for(int i = 0; i < negative.size(); i++) {
+        if(i + 1 < negative.size()) {
             sum += negative[i] * negative[i + 1];
             ++i;
-        }
-        else
+        } else {
             sum += negative[i];
+        }
     }
 
-    cout << sum << endl;
-
-    return 0;
+    cout << sum;
 }
